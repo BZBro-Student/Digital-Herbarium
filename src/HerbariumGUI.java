524b151a624741fed08ba6ca85466092685a1377
BZBro-Student <brodenbenson@u.boisestate.edu>
2025-10-15 14:40:33 -0600

    started project

--- src/HerbariumGUI.java
+++ src/HerbariumGUI.java
@@ -1,50 +1,96 @@
+import java.awt.GridLayout;
+
 import javax.swing.BoxLayout;
 import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 class HerbariumGui extends JPanel {
     private JPanel mainPanel;
     private JPanel rightPanel;
     private JPanel leftImagePanel;
+    private JPanel leftButtonPanel;
+    private JPanel subPanelOne;
+    private JPanel subPanelTwo;
+    private JPanel subPanelThree;
     private JLabel commonName;
     private JTextField commonNameField;
     private JLabel scientificName;
     private JTextField scientificNameField;
     private JLabel observations;
-    private JTextField observationsField;
+    private JTextArea observationsField;
+    private JScrollPane observationFieldPane;
     private ImageIcon flowerImage;
+    private JFileChooser pickFile;
+    private JButton save;
+    private JButton edit;
+
     HerbariumGui() {
-        
+        // Panel that will hold all other sub-panels
         mainPanel = new JPanel();
-        //Panel that will be on the left side of the mainPanel
+        mainPanel.setLayout(new BoxLayout(mainPanel, BoxLayout.X_AXIS));
+        // Panel that will be on the left side of the mainPanel
         rightPanel = new JPanel();
-        rightPanel.setLayout(new BoxLayout(rightPanel,BoxLayout.Y_AXIS ));
+        rightPanel.setLayout(new BoxLayout(rightPanel, BoxLayout.Y_AXIS));
+        subPanelOne = new JPanel();
+        subPanelTwo = new JPanel();
+        subPanelThree =  new JPanel();
         commonName = new JLabel("Common Name:");
         commonNameField = new JTextField(20);
         scientificName = new JLabel("Scientific Name:");
         scientificNameField = new JTextField(20);
         observations = new JLabel("Observations:");
-        observationsField = new JTextField(20);
-        //Adds to right Panel
-        rightPanel.add(commonName);
-        rightPanel.add(commonNameField);
-        rightPanel.add(scientificName);
-        rightPanel.add(scientificNameField);
-        rightPanel.add(observations);
-        rightPanel.add(observationsField);
-        //Panel that will contain plant image 
+        observationsField = new JTextArea();
+        observationFieldPane = new JScrollPane();
+        observationFieldPane.setViewportView(observationsField);
+        // Adds to right Panel
+        subPanelOne.add(commonName);
+        subPanelOne.add(commonNameField);
+        subPanelTwo.add(scientificName);
+        subPanelTwo.add(scientificNameField);
+        subPanelThree.add(observations);
+        subPanelThree.add(observationFieldPane);
+        subPanelThree.setLayout(new BoxLayout(subPanelThree, BoxLayout.Y_AXIS));
+        rightPanel.add(subPanelOne);
+        rightPanel.add(subPanelTwo);
+        rightPanel.add(subPanelThree);
+
+        // Panel that will contain plant image
         leftImagePanel = new JPanel();
-        //TO-DO make this so you can just pick a file
-        flowerImage = new ImageIcon("img/yippee-creature.gif");
-        //Add image to panel
+        leftImagePanel.setLayout(new BoxLayout(leftImagePanel, BoxLayout.Y_AXIS));
+        pickFile = new JFileChooser();
+        int result = pickFile.showOpenDialog(null);
+        // Pick image
+        if (result == JFileChooser.APPROVE_OPTION) {
+            flowerImage = new ImageIcon(pickFile.getSelectedFile().getAbsolutePath());
+        } else {
+            flowerImage = new ImageIcon("src/img/failedtoload.png");
+        }
+        // Add image to panel centered
         leftImagePanel.add(new JLabel(flowerImage));
 
+        // Panel that will contain save and edit buttons
+        leftButtonPanel = new JPanel();
+        save = new JButton("Save");
+        edit = new JButton("Edit");
 
+        // Add buttons to panel
+        leftButtonPanel.add(save);
+        leftButtonPanel.add(edit);
+
+        // Puts left button panel in correct place
+        leftImagePanel.add(leftButtonPanel);
+        // Puts left image panel in the right place
         mainPanel.add(leftImagePanel);
+        // Puts right panel in the right place
         mainPanel.add(rightPanel);
+        // puts panel into HerbariumGui
         this.add(mainPanel);
     }
-    
+
 }
